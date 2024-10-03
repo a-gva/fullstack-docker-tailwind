65fee78851c50fcdaa04c57b106389abd45dbd41
Alexy Viegas <167803977+alexyviegas-ab@users.noreply.github.com>
2024-10-02 21:20:45 -0300

    get users route - delete

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,4 +1,4 @@
-import { getUser } from '@/app/api/db/users';
+import { deleteUser, getUser, getUsers } from '@/app/api/db/users';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
@@ -12,6 +12,21 @@ export async function GET(req: NextRequest, context: { params: Params }) {
   try {
     const user = await getUser(userID);
     return NextResponse.json(user);
+  } catch (error: any) {
+    return NextResponse.json({
+      statusCode: 500,
+      message: error.message,
+      userID: userID || 'No user ID',
+    });
+  }
+}
+export async function DELETE(req: NextRequest, context: { params: Params }) {
+  const userID = context.params.id;
+
+  try {
+    await deleteUser(userID);
+    const users = await getUsers();
+    return NextResponse.json(users);
   } catch (error: any) {
     return NextResponse.json({
       statusCode: 500,
